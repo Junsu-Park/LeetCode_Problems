bb857071bb8395cd208d90340e81f89b7eba4358
Junsu-Park <40886318+Junsu-Park@users.noreply.github.com>
2022-07-01 10:00:00 +0900

    Time: 128 ms (22.32%), Space: 14.1 MB (10.28%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from collections import deque
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        L1 = deque([])
+        L2 = deque([])
+        while True:
+            L1.appendleft(str(l1.val))
+            if l1.next:
+                l1 = l1.next
+            else:break
+        while True:
+            L2.appendleft(str(l2.val))
+            if l2.next:
+                l2 = l2.next
+            else:break
+        ans = list(str(int(''.join(L1)) + int(''.join(L2))))
+        tmp_node = ListNode(int(ans.pop()))
+        start_node = tmp_node
+        while ans:
+            next_node = ListNode(int(ans.pop()))
+            tmp_node.next = next_node
+            tmp_node = next_node
+        
+        return start_node
+        
