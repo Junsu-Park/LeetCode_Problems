4ae83f20f5d36faffe0eeb53d6d881d4f3bdc543
Junsu-Park <40886318+Junsu-Park@users.noreply.github.com>
2022-07-19 21:28:01 +0900

    Time: 88 ms (40.59%), Space: 14 MB (70.05%) - LeetHub

--- 223-rectangle-area/223-rectangle-area.py
+++ 223-rectangle-area/223-rectangle-area.py
@@ -1,13 +1,5 @@
-def check_area(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
-    if ax2 > bx1 and bx2 > ax1 and ay2 > by1 and by2 > ay1:
-        return True
-    return False
+
 
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        basic_area = (ax2-ax1) * (ay2-ay1) + (bx2-bx1) * (by2-by1)
-        return basic_area - max(0, min(ay2, by2) - max(ay1, by1)) * max(0, min(ax2, bx2) - max(ax1, bx1))
-        # if check_area(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
-        #     return basic_area - (min(ay2, by2) - max(ay1, by1)) * (min(ax2, bx2) - max(ax1, bx1))
-        # else:
-        #     return basic_area
+        return (ax2-ax1) * (ay2-ay1) + (bx2-bx1) * (by2-by1) - max(0, min(ay2, by2) - max(ay1, by1)) * max(0, min(ax2, bx2) - max(ax1, bx1))
